920e9855dbf6eddf424a036a555558d2aa6ff0f4
Esteban S <esteban.develops@gmail.com>
2022-07-15 22:48:29 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Global Variables
 const mouse = document.querySelector(".cursor");
-const countDownDate = new Date("Aug 14, 2021 19:00:00").getTime();
+const countDownDate = new Date("Aug 14, 2022 19:00:00").getTime();
 
 // Functions
 function cursor(e) {
